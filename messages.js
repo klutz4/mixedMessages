4baff66540a177044dd7903e1c14be0e1a1aa3e2
Kelly Lutz <kellylutz@Kellys-MacBook-Air.local>
2024-05-24 13:47:08 -0600

    Create message part options

---
+++ messages.js
@@ -0,0 +1,7 @@
+
+// lists of inputs for the message
+const events = ['find 3 kittens','meet your soulmate', 'trip over nothing', 'get the call of a lifetime', 'witness a car accident', 'miss your parents', 'win the lottery'];
+const locations = ['the bookstore', 'the coffee shop', 'the library', 'your home', 'school', 'the grocery store', 'the park'];
+const feelings = ['elated', 'angry', 'excited', 'confused', 'frustrated', 'ecstatic', 'dumbfounded', 'neutral', 'devastated'];
+
+
