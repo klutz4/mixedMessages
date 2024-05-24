5baeada7770467215fb5217324791e2ec8e3d9f3
Kelly Lutz <kellylutz@Kellys-MacBook-Air.local>
2024-05-24 13:54:58 -0600

    Add functionality and testing

--- messages.js
+++ messages.js
@@ -5,3 +5,21 @@ const locations = ['the bookstore', 'the coffee shop', 'the library', 'your home
 const feelings = ['elated', 'angry', 'excited', 'confused', 'frustrated', 'ecstatic', 'dumbfounded', 'neutral', 'devastated'];
 
 
+// functions for the game
+const generateMessagePart = (messagePart) => {
+    if (messagePart === 'event') {
+        return events[Math.floor(Math.random() * events.length)]
+    } else if (messagePart === 'location') {
+        return locations[Math.floor(Math.random() * events.length)]
+    } else if (messagePart === 'feeling') {
+        return feelings[Math.floor(Math.random() * events.length)]
+    }
+}
+
+const generateNewMessage = () => {
+    return `You will ${generateMessagePart('event')} at ${generateMessagePart('location')} and you will feel ${generateMessagePart('feeling')}!`
+}
+
+
+//  testing
+console.log(generateNewMessage());
